231b14c1bc0772e1389779933f4025c25aed302f
babakfp <babak.bxf@gmail.com>
2022-01-08 13:50:46 -0800

    Added blog page

---
+++ src/routes/blog/index.svelte
@@ -0,0 +1,24 @@
+<script>
+	import PostCard from '$lib/PostCard.svelte'
+	import Pagination from '$lib/Pagination.svelte'
+</script>
+
+<div class="grid gap-4 grid-cols-2 sm:gap-6 sm:grid-cols-3 md2:grid-cols-4 lg:grid-cols-3 2lg:grid-cols-4 xl:grid-cols-5 3xl:grid-cols-6">
+	<PostCard />
+	<PostCard />
+	<PostCard />
+	<PostCard />
+	<PostCard />
+	<PostCard />
+	<PostCard />
+	<PostCard />
+	<PostCard />
+	<PostCard />
+	<PostCard />
+	<PostCard />
+	<PostCard />
+	<PostCard />
+	<PostCard />
+</div>
+
+<Pagination />
