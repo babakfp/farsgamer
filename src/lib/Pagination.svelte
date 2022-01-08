049ebeaf2a04822517d5078ce92aa403b34c1523
babakfp <babak.bxf@gmail.com>
2022-01-08 01:49:28 -0800

    dsf

---
+++ src/lib/Pagination.svelte
@@ -0,0 +1,43 @@
+<ul class="flex items-center justify-center gap-2 mt-12">
+	<li>
+		<button class="pg-btn pg-btn--prev-next">
+			<i class="fi-rr-angle-double-small-right"></i>
+		</button>
+	</li>
+	<li>
+		<ul class="flex gap-2">
+			<li>
+				<a class="pg-btn pg-btn--num" href="javascript:">1</a>
+			</li>
+			<li>
+				<a class="pg-btn pg-btn--num pg-btn--num-active" href="javascript:">2</a>
+			</li>
+			<li class="pg-btn">
+				<i class="fi-rr-menu-dots"></i>
+			</li>
+			<li>
+				<a class="pg-btn pg-btn--num" href="javascript:">9</a>
+			</li>
+		</ul>
+	</li>
+	<li>
+		<button class="pg-btn pg-btn--prev-next">
+			<i class="fi-rr-angle-double-small-left"></i>
+		</button>
+	</li>
+</ul>
+
+<style lang="postcss">
+	.pg-btn {
+		@apply flex items-center justify-center w-10 h-10 text-gray-500 rounded-sm duration-200 ease-in-out;
+	}
+
+	.pg-btn--prev-next,
+	.pg-btn--num {
+		@apply bg-gray-200 hover:bg-brand hover:text-white;
+	}
+
+	.pg-btn--num-active {
+		@apply bg-brand text-white pointer-events-none;
+	}
+</style>
---
+++ src/routes/shop/index.svelte
@@ -0,0 +1,24 @@
+<script>
+	import SelectBoxes from './lib/SelectBoxes.svelte'
+	import ProductCard from '$lib/ProductCard.svelte'
+	import Pagination from '$lib/Pagination.svelte'
+</script>
+
+<SelectBoxes />
+
+<div class="grid grid-cols-2 gap-4 mt-8 sm:gap-6 sm:grid-cols-3 md2:grid-cols-4 lg:grid-cols-3 lg2:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+	<ProductCard />
+	<ProductCard />
+	<ProductCard />
+	<ProductCard />
+	<ProductCard />
+	<ProductCard />
+	<ProductCard />
+	<ProductCard />
+	<ProductCard />
+	<ProductCard />
+	<ProductCard />
+	<ProductCard />
+</div>
+
+<Pagination />
