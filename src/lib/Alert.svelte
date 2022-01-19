b4b1788edfe88723d9672bdfa28178ddbb7a28b6
babakfp <babak.bxf@gmail.com>
2022-01-19 00:23:41 -0800

    Added Alert component

---
+++ src/lib/Alert.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let content
+	export let type
+</script>
+
+<div class="flex items-center gap-4 px-4 py-3 rounded border
+	{!type && 'bg-gray-50 text-gray-900 border-gray-200'}
+	{type === 'success' && 'bg-green-50 text-green-900 border-green-200'}
+	{type === 'error' && 'bg-red-50 text-red-900 border-red-200'}
+	{type === 'info' && 'bg-sky-50 text-sky-900 border-sky-200'}
+	{type === 'warn' && 'bg-yellow-50 text-yellow-900 border-yellow-200'}
+">
+	<i class="fi-rr-check text-sm"></i>
+	{content}
+</div>
