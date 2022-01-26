b72ab5a8723fda206d34093e1f248dcb90cd12c6
babakfp <babak.bxf@gmail.com>
2022-01-26 13:21:01 -0800

    Sepereated product gallery thumbs into its components

---
+++ src/routes/shop/[product_id]/lib/GalleryMainThumbs.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { Swiper, SwiperSlide } from 'swiper/svelte'
+	import { Pagination, Autoplay, Thumbs } from 'swiper'
+
+	export let thumbsSwiper
+
+	const posters = [
+		{ imgSrc: '/img/products/product-thumb (1).png' },
+		{ imgSrc: '/img/products/product-thumb (2).png' },
+		{ imgSrc: '/img/products/product-thumb (3).png' },
+		{ imgSrc: '/img/products/product-thumb (4).png' },
+		{ imgSrc: '/img/products/product-thumb (5).png' },
+	]
+</script>
+
+<Swiper
+	class="w-62 h-62 mr-auto ml-0"
+	modules={[ Pagination, Autoplay, Thumbs ]}
+	thumbs={{ swiper: thumbsSwiper }}
+	speed={600}
+	grabCursor={true}
+	pagination={{ clickable: true, dynamicBullets: true }}
+	autoplay={{ delay: 4000, disableOnInteraction: false }}
+	spaceBetween={16}
+>
+	{#each posters as poster}
+		<SwiperSlide>
+			<a href={poster.href || 'javascript:'}>
+				<img class="rounded" src={poster.imgSrc} alt />
+			</a>
+		</SwiperSlide>
+	{/each}
+</Swiper>
