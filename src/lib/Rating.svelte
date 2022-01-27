e25cfbd8e64091b973684cf85d64a42771ca8824
babakfp <babak.bxf@gmail.com>
2022-01-27 05:47:13 -0800

    Rating better UI

--- src/lib/Rating.svelte
+++ src/lib/Rating.svelte
@@ -5,11 +5,11 @@
 <ol class="flex gap-1">
 	{#each [...Array(5).keys()] as i}
 		<li>
-			<label class="relative group">
+			<label class="group relative flex items-center justify-center w-8 h-8 {value >= i + 1 ? 'bg-yellow bg-opacity-5' : 'bg-gray-50'} rounded">
 				<input class="hidden" type="radio"name="rating" bind:group={value} value={ i + 1 }>
-				<i class="{value >= i + 1 ? 'fi-sr-star text-yellow' : 'fi-rr-star text-gray-400' }"></i>
+				<i class="{value >= i + 1 ? 'fi-sr-star text-yellow' : 'fi-rr-star text-gray-500' }"></i>
 
-				<span class="absolute opacity-0 w-6 h-6 flex items-center justify-center bg-gray-100 rounded-sm -top-7 translate-x-1/2 right-1/2 text-xs duration-200 ease-in-out group-hover:opacity-100">{ i + 1 }</span>
+				<span class="absolute opacity-0 w-12 h-6 flex items-center justify-center bg-gray-50 rounded-sm -top-7 translate-x-1/2 right-1/2 text-2xs duration-200 ease-in-out group-hover:opacity-100">{ i + 1 } ستاره</span>
 			</label>
 		</li>
 	{/each}
