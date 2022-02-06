dc807bbc08570aab8a9c0316019b5004dbbf4e6f
babakfp <babak.bxf@gmail.com>
2022-02-06 12:57:22 -0800

    PostCard hover rotate

--- src/lib/PostCard.svelte
+++ src/lib/PostCard.svelte
@@ -1,4 +1,4 @@
-<a class="grid p-2 text-center rounded bg-white duration-200 ease-in-out hover:shadow-sm hover:shadow-gray-200 hover:scale-105" href="/blog/1">
+<a class="grid p-2 text-center rounded bg-white duration-200 ease-in-out hover:shadow-sm hover:shadow-gray-200 hover:scale-105 hover:rotate-2" href="/blog/1">
 	<img class="rounded" src="/img/products/product-thumb (1).png" alt>
 	<div class="p-2 mt-2">
 		<h3 class="text-sm font-bold line-clamp-1">گیف کارد 20 دلاری استیم</h3>
