149aed1b05a2de53b5b830ba55017dde951b8188
babakfp <babak.bxf@gmail.com>
2022-02-06 12:52:15 -0800

    Spinner

--- src/routes/stuff/index.svelte
+++ src/routes/stuff/index.svelte
@@ -1,22 +1,9 @@
-<div class="py-8 grid gap-4">
-  <button class="btn btn--brand h-12">
-    <i class="icon-circle-dashed animate-spin"></i>
-    <span>در حال بارگذاری</span>
-  </button>
-  <button class="btn btn--brand h-12">
-    <i class="icon-circle-half animate-spin"></i>
-    <span>در حال بارگذاری</span>
-  </button>
-  <button class="btn btn--brand h-12">
-    <i class="icon-circle-quarter animate-spin"></i>
-    <span>در حال بارگذاری</span>
-  </button>
-  <button class="btn btn--brand h-12">
-    <i class="icon-circle-three-quarters animate-spin"></i>
-    <span>در حال بارگذاری</span>
-  </button>
-  <button class="btn btn--brand h-12">
-    <i class="icon-spinner animate-spin"></i>
-    <span>در حال بارگذاری</span>
-  </button>
-</div>
+<button class="btn btn--brand h-12">
+	<i class="fi-rr-spinner animate-spin"></i>
+	<span>در حال بارگذاری</span>
+</button>
+
+<button class="btn btn--brand h-12">
+	<i class="fi-rr-spinner-alt animate-spin"></i>
+	<span>در حال بارگذاری</span>
+</button>
