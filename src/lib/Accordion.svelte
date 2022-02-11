fc0625e3a76627c595065f48e3b3ce355b07654c
babakfp <babak.bxf@gmail.com>
2022-02-11 09:10:17 -0800

    Accordion stuff

--- src/lib/Accordion.svelte
+++ src/lib/Accordion.svelte
@@ -9,7 +9,7 @@
 		<i class="fi-rr-caret-down text-gray-400"></i>
 	</button>
 
-	<div class="Accordion-Body relative -z-1 -mt-4 overflow-hidden duration-300 ease-in-out">
+	<div class="Accordion-Body relative -z-1 -mt-4 duration-300 ease-in-out">
 		<div class="p-4 pt-8 bg-white border-2 border-gray-200 rounded-b text-sm">
 			<slot />
 		</div>
@@ -18,10 +18,7 @@
 </li>
 
 <style lang="postcss">
-	.Accordion-Body {
-		@apply h-0 pointer-events-none;
-	}
-	:global(.Accordion.Accordion--Active) .Accordion-Body {
-		@apply h-auto;
+	:global(.Accordion:not(.Accordion--Active)) .Accordion-Body {
+		@apply h-0 overflow-hidden pointer-events-none;
 	}
 </style>
