80ce73db6c37af7ef84158dc7ce3e7a1514f8123
babakfp <babak.bxf@gmail.com>
2022-02-13 14:35:48 -0800

    Removed unneeded TPAlert

--- src/layouts/HeaderMmenu.svelte
+++ src/layouts/HeaderMmenu.svelte
@@ -2,13 +2,11 @@
 	import { browser } from '$app/env'
 	import Header from '$lib/header/Index.svelte'
 	import SideMenu from '$lib/sidemenu/Index.svelte'
-	import TPAlert from '$lib/TPAlert.svelte'
 	import Footer from '$lib/footer/Index.svelte'
 </script>
 
 <Header />
 <SideMenu />
-<TPAlert />
 
 <main id="main">
 	<div id="page-content" class="relative p-4 overflow-x-hidden sm:p-6 2xl:p-8 2xl:pb-12">
