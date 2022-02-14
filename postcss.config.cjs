516f36d65f9af1a5516b6453206db1e4108d92b9
babakfp <babak.bxf@gmail.com>
2022-02-13 16:43:37 -0800

    Removed tailwindcss/nesting

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,7 +3,6 @@ const dev = process.env.NODE_ENV === 'development'
 module.exports = {
   plugins: [
 		require('postcss-import'),
-		require('tailwindcss/nesting'),
     require('tailwindcss'),
     !dev && require('autoprefixer'),
   ],
