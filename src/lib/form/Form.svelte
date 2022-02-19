442e4ee74f66ed13700c9d08366c6baa9cf99a13
babakfp <babak.bxf@gmail.com>
2022-02-18 22:13:51 -0800

    Form.svelte stuff

---
+++ src/lib/form/Form.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { setContext } from 'svelte'
+
+	let className = ''
+	export {className as class}
+	
+	export let isValid = true
+	setContext('isValid', isValid)
+</script>
+
+<form class={className} on:submit|preventDefault>
+	<slot />
+</form>
--- src/routes/login/index.svelte
+++ src/routes/login/index.svelte
@@ -1,4 +1,7 @@
 <script>
+	import { goto } from '$app/navigation'
+
+	import Form from '$lib/form/Form.svelte'
 	import Input from '$lib/Input.svelte'
 	import Checkbox from '$lib/Checkbox.svelte'
 
@@ -13,24 +16,15 @@
 	<h2 class="text-lg font-bold">ورود به حساب</h2>
 	
 	{#if state === 'first'}
-		<form class="space-y-6 mt-4 p-6 bg-white rounded" on:submit={_=> state = 'second'}>
-			
-			<Input
-				label="شماره موبایل"
-				class="dir-ltr" type="tel" name="phone" autocomplete="tel" placeholder="09..."
-			/>
-
+		<Form class="space-y-6 mt-4 p-6 bg-white rounded" on:submit={_=> state = 'second'}>
+			<Input label="شماره موبایل" class="dir-ltr" type="tel" name="phone" autocomplete="tel" placeholder="09..." />
 			<Checkbox name="rememberme">مرا به خاطر داشته باش</Checkbox>
-
 			<button class="btn btn--submit btn--brand w-full">ادامه فرایند ثبت‌نام</button>
-		</form>
+		</Form>
 	{/if}
 
-	<!--  -->
-
 	{#if state === 'second'}
-		<form class="space-y-6 mt-4 p-6 bg-white rounded">
-
+		<Form class="space-y-6 mt-4 p-6 bg-white rounded" on:submit={_=> goto('/dashboard')}>
 			<div>
 				<label for="code">کد تایید</label>
 				<input class="input text-center tracking-3 input-no-arrows" type="number" name="code" minlength="5" maxlength="5" placeholder="_ _ _ _ _">
@@ -43,8 +37,7 @@
 			<div class="text-sm text-center">
 				<span>ارسال مجدد کد بعد از</span> <time>(2:00)</time>
 			</div>
-
-		</form>
+		</Form>
 	{/if}
 
 </div>
