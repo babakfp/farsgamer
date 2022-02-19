1ba91ece0f7f54604697718bc11a725789bfb7a3
babakfp <babak.bxf@gmail.com>
2022-02-19 06:12:46 -0800

    Form.svelte ContextAPI

--- src/lib/form/Form.svelte
+++ src/lib/form/Form.svelte
@@ -1,13 +1,17 @@
 <script>
 	import { setContext } from 'svelte'
+	import { writable } from 'svelte/store'
 
 	let className = ''
-	export {className as class}
-	
-	export let isValid = true
+	export { className as class }
+
+	export let isValid = writable(true)
 	setContext('isValid', isValid)
 </script>
 
-<form class={className} on:submit|preventDefault>
+<form
+	class="{className} {isValid ? 'is-valid' : 'not-valid'}"
+	on:submit|preventDefault
+>
 	<slot />
 </form>
