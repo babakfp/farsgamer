ba2adbfafd795d4f1c478128670b4636d6912e6c
babakfp <96848024+windyclory@users.noreply.github.com>
2022-02-21 13:24:32 -0800

    Burger lighter

--- src/lib/header/Burger.svelte
+++ src/lib/header/Burger.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <button on:click={$page.url.pathname.includes('/dashboard') ? dashboardToggle() : toggle()} class="js-burger-toggle-btn flex items-center justify-center w-16 h-16 -mr-4" id="burger">
-	<i class="fi-rr-grid duration-300 ease-in-out {$isOpen && 'rotate-45'} sm:text-lg"></i>
+	<i class="fi-rr-grid text-gray-600 duration-300 ease-in-out {$isOpen && 'rotate-45'} sm:text-lg"></i>
 </button>
 
 <style lang="postcss">
