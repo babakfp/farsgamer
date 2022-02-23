c7640a035e1126b36a241a13c98755a95b12f0c2
babakfp <96848024+windyclory@users.noreply.github.com>
2022-02-23 12:08:10 -0800

    nav item icon opacity

--- src/lib/sidemenu/NavItem.svelte
+++ src/lib/sidemenu/NavItem.svelte
@@ -16,7 +16,7 @@
 		"
 		{href}
 	>
-		<i class="{icon} w-8 h-8 flex justify-center items-center text-lg"></i>
+		<i class="{icon} w-8 h-8 flex justify-center items-center text-lg opacity-75"></i>
 		<span class="font-medium">{ title }</span>
 	</a>
 </li>
