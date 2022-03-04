ca0c2b5eef80c7a40f6d8d89a90b8b08de90808e
babakfp <44144724+babakfp@users.noreply.github.com>
2022-03-03 21:53:36 -0800

    Removed unneeded layout stuff

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,31 +4,16 @@
 	import '../app.css'
 
 	import LoadingBar from '$lib/LoadingBar.svelte'
-	
+
 	// Layouts
-	import Default from '$layouts/Default.svelte'
 	import HeaderMmenu from '$layouts/HeaderMmenu.svelte'
-	// import Dashboard from '$layouts/Dashboard.svelte'
-	
+
 	const layouts = {
 		// '<route>': <Layout Component>,
-		// '/dashboard': Dashboard,
 	}
 	$: layout = layouts[$page.url.pathname] ?? HeaderMmenu
 
-	// let pageClass = ''
 	$: {
-		// if ( $page.url.pathname === '/' ) {
-		// 	pageClass = 'page-home'
-		// } else {
-		// 	let pathnameWithoutFirstSlash = $page.url.pathname.replace('/', '')
-		// 	let pathnameAllSlashToDash = pathnameWithoutFirstSlash.replaceAll('/', '-')
-		// 	pageClass = `page-${pathnameAllSlashToDash}`
-		// }
-		
-		// browser && document.documentElement.removeAttribute('class')
-		// browser && document.documentElement.classList.add(pageClass)
-
 		// Because of the dashboard layout we need to add a class to dashboard pages so we be able to control the style of some elemetns.
 		if ( $page.url.pathname.includes('/dashboard') ) {
 			browser && document.body.classList.add('dashboard-header')
