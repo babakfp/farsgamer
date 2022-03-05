e28bdb30cb195d84b14776c22df255dee5a063f6
babakfp <44144724+babakfp@users.noreply.github.com>
2022-03-04 22:16:10 -0800

    Breadcrumb

--- src/lib/Breadcrumb.svelte
+++ src/lib/Breadcrumb.svelte
@@ -2,24 +2,21 @@
 	export let items = []
 </script>
 
-<ul class="breadcrumb">
+<ul class="flex flex-wrap items-center text-gray-500 text-sm">
 	{#each items as item, i}
 		<li class="inline-flex items-center">
 			{#if item.href}
 				<a class="link-transition hover:text-brand" href={item.href}>{item.title}</a>
 			{:else}
-				<span>{ item.title }</span>
+				<span class="text-gray-400">{ item.title }</span>
 			{/if}
 		</li>
 	{/each}
 </ul>
 
 <style lang="postcss">
-	.breadcrumb {
-		@apply flex flex-wrap items-center text-gray-500 text-sm;
-	}
 	li:not(:last-child)::after {
 		content: "/";
-		@apply text-xs text-gray-300 mx-2;
+		@apply text-xs text-gray-300 mx-1;
 	}  
 </style>
