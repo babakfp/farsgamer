7a3ea58c2d522e68a844409473868488a4996b12
babakfp <44144724+babakfp@users.noreply.github.com>
2022-03-09 12:56:39 -0800

    user reviews

--- src/lib/home/Comment.svelte
+++ src/lib/home/Comment.svelte
@@ -4,7 +4,7 @@
 
 <div class="relative p-4 pb-3 bg-white rounded text-sm">
 	<div class="quote-icon absolute left-2 top-2 opacity-40">
-		<i class="icon-quote text-3xl"></i>
+		<i class="icon-quote text-3xl opacity-50"></i>
 	</div>
 
 	<p class="line-clamp-3">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.<p>
