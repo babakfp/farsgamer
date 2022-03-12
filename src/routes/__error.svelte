b80bd6ef406f23678a470c26ba48824efc284025
babakfp <44144724+babakfp@users.noreply.github.com>
2022-03-12 11:40:33 -0800

    404 error page

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -19,13 +19,17 @@
 
 <div class="py-32 -mb-4">
 	<h1 class="text-center font-bold text-4xl">{status}</h1>
-	<h2 class="text-center">{error.message}</h2>
 
-	{#if error.stack && dev}
-		<code class="scrollbar scrollbar-x-spacing-lg block mt-8 p-4 bg-gray-200 rounded dir-ltr lg:p-8">
-			<pre>
-				{error.stack}
-			</pre>
-		</code>
+	{#if status === 404}
+		<p class="text-center">صفحه‌ای که به دنبال آن میگردید, پیدا نشد.</p>
+	{:else}
+		<h2 class="text-center">{error.message}</h2>
+		{#if error.stack && dev}
+			<code class="scrollbar scrollbar-x-spacing-lg block mt-8 p-4 bg-gray-200 rounded dir-ltr lg:p-8">
+				<pre>
+					{error.stack}
+				</pre>
+			</code>
+		{/if}
 	{/if}
 </div>
