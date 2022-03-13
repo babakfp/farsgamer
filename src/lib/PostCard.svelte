c1a5d99b89d5252aa69282ebaa1d023d37b7091f
babakfp <44144724+babakfp@users.noreply.github.com>
2022-03-12 16:17:44 -0800

    fixes post card for dnamic data

--- src/lib/PostCard.svelte
+++ src/lib/PostCard.svelte
@@ -1,11 +1,11 @@
 <script>
+	export let id
 	export let title
-	export let href
-	export let src
+	export let featuredImage
 </script>
 
-<a class="grid p-2 text-center rounded bg-white duration-200 ease-in-out hover:shadow-sm hover:scale-105 hover:rotate-2" {href}>
-	<img class="rounded" {src} alt>
+<a class="grid p-2 text-center rounded bg-white duration-200 ease-in-out hover:shadow-sm hover:scale-105 hover:rotate-2" href="/blog/{id}">
+	<img class="rounded" src={featuredImage} alt>
 	<div class="p-2 mt-2">
 		<h3 class="text-sm font-bold line-clamp-1">{title}</h3>
 	</div>
