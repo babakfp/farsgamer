676822f162c8341027fa1abcb9c72c45122e9aeb
babakfp <44144724+babakfp@users.noreply.github.com>
2022-03-13 10:11:53 -0700

    removed stuff

--- src/routes/stuff/index.svelte
+++ src/routes/stuff/index.svelte
@@ -2,13 +2,3 @@
 	<i class="fi-rr-spinner animate-spin"></i>
 	<span>در حال بارگذاری</span>
 </button>
-
-<button class="btn btn--brand h-12">
-	<i class="fi-rr-spinner-alt animate-spin"></i>
-	<span>در حال بارگذاری</span>
-</button>
-
-<button class="btn btn--brand h-12">
-	<i class="icon-circle-dashed animate-spin"></i>
-	<span>در حال بارگذاری</span>
-</button>
