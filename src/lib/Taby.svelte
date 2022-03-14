4135147832d78ec4ac9565ceec6c4d5504484719
babakfp <44144724+babakfp@users.noreply.github.com>
2022-03-13 18:43:29 -0700

    Fixed tabs scrollar

--- src/lib/Taby.svelte
+++ src/lib/Taby.svelte
@@ -10,9 +10,15 @@
 </script>
 
 <div bind:this={tabyEl}>
-	<ul class="{className} flex items-center">
+	<ul class="{className} tabs flex items-center pb-2 -mb-2 scrollbar-x scrollbar-x-spacing-lg overflow-y-hidden">
 		<slot name="tab" />
 	</ul>
 
 	<slot name="body" />
 </div>
+
+<style lang="postcss">
+	.tabs::-webkit-scrollbar-thumb {
+		@apply !bg-gray-200;
+	}
+</style>
