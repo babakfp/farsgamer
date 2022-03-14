7d9f81302aee2e617f3dc63f294127ed21c3dbfa
babakfp <44144724+babakfp@users.noreply.github.com>
2022-03-13 19:07:47 -0700

    fixed blog posts

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,4 +1,5 @@
 <script>
+	import posts from '$db/posts.js'
 	import PostCard from '$lib/PostCard.svelte'
 	import Pagination from '$lib/Pagination.svelte'
 </script>
@@ -8,18 +9,15 @@
 </svelte:head>
 
 <div class="grid gap-4 grid-cols-2 sm:gap-6 sm:grid-cols-3 2md:grid-cols-4 lg:grid-cols-3 2lg:grid-cols-4 xl:grid-cols-5 3xl:grid-cols-6">
-	<PostCard />
-	<PostCard />
-	<PostCard />
-	<PostCard />
-	<PostCard />
-	<PostCard />
-	<PostCard />
-	<PostCard />
-	<PostCard />
-	<PostCard />
-	<PostCard />
-	<PostCard />
+	{#each posts as post}
+		<PostCard {...post} />
+	{/each}
+	{#each posts as post}
+		<PostCard {...post} />
+	{/each}
+	{#each posts as post}
+		<PostCard {...post} />
+	{/each}
 	<PostCard />
 	<PostCard />
 	<PostCard />
