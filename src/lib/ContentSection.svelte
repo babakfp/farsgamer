8b2717873c029e814163760bd998a9ff390ee708
babakfp <44144724+babakfp@users.noreply.github.com>
2022-03-21 01:07:25 -0700

    btn style chage

--- src/lib/ContentSection.svelte
+++ src/lib/ContentSection.svelte
@@ -7,9 +7,9 @@
   <div class="flex items-center justify-between mb-4">
 		<h2 class="font-bold text-lg">{title}</h2>
 		{#if href}
-			<a class="btn btn--sm btn--outline btn--brand" {href}>
+			<a class="btn btn--sm btn--gray btn--outline gap-2" {href}>
 				<span>مشاهده همه</span>
-				<i class="fi-rr-angle-left !text-xs"></i>
+				<i class="fi-rr-angle-double-small-left !text-sm"></i>
 			</a>
 		{/if}
   </div>
