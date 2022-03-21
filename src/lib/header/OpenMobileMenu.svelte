0a6bc4228b233ba4cc21d126dbe868aae1760188
babakfp <44144724+babakfp@users.noreply.github.com>
2022-03-20 22:52:48 -0700

    animate-pulse and active:scale

--- src/lib/header/OpenMobileMenu.svelte
+++ src/lib/header/OpenMobileMenu.svelte
@@ -3,7 +3,7 @@
 	import { isOpen, toggle, dashboardToggle } from '$store/mobile-menu.js'
 </script>
 
-<button on:click={$page.url.pathname.includes('/dashboard') ? dashboardToggle() : toggle()} class="js-burger-toggle-btn flex items-center justify-center w-16 h-16 -mr-4" id="burger">
+<button on:click={$page.url.pathname.includes('/dashboard') ? dashboardToggle() : toggle()} class="js-burger-toggle-btn flex items-center justify-center w-16 h-16 -mr-4 active:scale-95" id="burger">
 	<i class="fi-rr-grid text-gray-600 duration-300 ease-in-out {$isOpen && 'rotate-45'} sm:text-lg"></i>
 </button>
 
