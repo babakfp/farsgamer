80677b3a0116f6611711d99cf1cd3532dfbba434
babakfp <44144724+babakfp@users.noreply.github.com>
2022-03-25 22:03:38 -0700

    formatted

--- src/utils/tailwind/utilities/overflow.cjs
+++ src/utils/tailwind/utilities/overflow.cjs
@@ -2,13 +2,13 @@ const plugin = require('tailwindcss/plugin')
 
 const overflow = plugin( ({ addUtilities }) => {
 	addUtilities({
-		'.overflow-overlay': { 'overflow': 'overlay' },
-		'.overflow-y-overlay': { 'overflow-y': 'overlay' },
-		'.overflow-x-overlay': { 'overflow-x': 'overlay' },
+		'.overflow-overlay': 		{ 'overflow': 	'overlay' },
+		'.overflow-y-overlay': 	{ 'overflow-y': 'overlay' },
+		'.overflow-x-overlay': 	{ 'overflow-x': 'overlay' },
 
-		'.overflow-unset': { 'overflow': 'unset' },
-		'.overflow-y-unset': { 'overflow-y': 'unset' },
-		'.overflow-x-unset': { 'overflow-x': 'unset' },
+		'.overflow-unset': 			{ 'overflow': 	'unset' 	},
+		'.overflow-y-unset': 		{ 'overflow-y': 'unset' 	},
+		'.overflow-x-unset': 		{ 'overflow-x': 'unset' 	},
 	})
 })
 
