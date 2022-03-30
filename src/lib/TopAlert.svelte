b197653351427e79b174f2707663fc3324559dc3
babakfp <44144724+babakfp@users.noreply.github.com>
2022-03-29 18:31:33 -0700

    Make TopAlert not sticky

--- src/lib/TopAlert.svelte
+++ src/lib/TopAlert.svelte
@@ -5,14 +5,12 @@
 </script>
 
 {#if $showTopAlert && !$isSideMenuOpen}
-	<div class="z-10 sticky top-0 shadow-b-sm-muted">
-		<div class="flex p-4 bg-white bg-opacity-75 backdrop-blur-md">
-			<button class="h-full p-4 -mr-4 -my-4" on:click={_=> $showTopAlert = false}>
-				<i class="fi-rr-cross-small"></i>
-			</button>
-			<div>
-				<p class="text-sm">پشتیبانی 24 ساعته از طریق چت آنلاین در خدمت شما عزیزان. جهت اطلاع از آخرین اخبار <a class="link link--underline" href="javascript:">اینستاگرام فارس گیمر</a> را دنبال کنید.</p>
-			</div>
+	<div class="flex p-4 bg-white">
+		<button class="h-full p-4 -mr-4 -my-4" on:click={_=> $showTopAlert = false}>
+			<i class="fi-rr-cross-small"></i>
+		</button>
+		<div>
+			<p class="text-sm">پشتیبانی 24 ساعته از طریق چت آنلاین در خدمت شما عزیزان. جهت اطلاع از آخرین اخبار <a class="link link--underline" href="javascript:">اینستاگرام فارس گیمر</a> را دنبال کنید.</p>
 		</div>
 	</div>
 {/if}
