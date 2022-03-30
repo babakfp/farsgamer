1a7b1e42a187f35386f8d79a06c617c020033f11
babakfp <44144724+babakfp@users.noreply.github.com>
2022-03-30 00:43:10 -0700

    Fixed TopAlert glitch

--- src/lib/TopAlert.svelte
+++ src/lib/TopAlert.svelte
@@ -1,11 +1,12 @@
 <script>
+	import { browser, dev, mode } from '$app/env'
 	import { writable } from 'svelte-local-storage-store'
 	import { isOpen as isSideMenuOpen } from '$store/mobile-menu.js'
 
 	const showTopAlert = writable('showTopAlert', true)
 </script>
 
-{#if $showTopAlert && !$isSideMenuOpen}
+{#if browser && $showTopAlert && !$isSideMenuOpen}
 	<div id="TopAlert" class="flex items-center p-4 bg-white border-b border-gray-100">
 		<button class="p-4 -mr-4 -my-4 duration-150 hover:text-brand hover:scale-110"
 			on:click={_=> $showTopAlert = false}
