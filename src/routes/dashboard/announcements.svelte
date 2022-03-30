06a86281977001ffa18c1a3fc6d9e3b6725e1caa
babakfp <44144724+babakfp@users.noreply.github.com>
2022-03-30 16:41:28 -0700

    dashaord anns redesign

--- src/routes/dashboard/announcements.svelte
+++ src/routes/dashboard/announcements.svelte
@@ -1,29 +1,21 @@
-<div class="grid gap-4">
+<script>
+	import { announcements } from '$store/announcements.js'
+	import Alert from '$lib/Alert.svelte'
+</script>
 
-	<div class="comment">
-		<div class="comment__head">
-			<div class="comment__info">
-				<!-- Date -->
-				<div class="comment__date">
-					<span class="comment__date-day">29</span>
-					<span class="comment__date-month">اوردیبهشت</span>
+{#if $announcements && $announcements.length > 0}
+	<ul class="grid gap-4">
+		{#each $announcements as ann}
+			<li class="grid p-4 bg-white rounded">
+				<div class="flex items-center justify-between">
+					<span class="font-bold text-sm line-clamp-1">{ann.title}</span>
+					<span class="text-2xs text-gray-500">۲ روز پیش</span>
 				</div>
-				<div class="comment__user">
-					<!-- user f and l name -->
-					<span class="comment__user-name">محصوالات جدید</span>
-				</div>
-			</div>
-			<!-- Tags -->
-			<div class="comment__tags">
-				<span class="comment__tag text-primary">اطلاعیه عمومی</span>
-			</div>
-		</div>
-		<div class="comment__body">
-			<div class="comment__content">
-				<!-- actual content of the comment -->
-				<p class="comment__message">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم لورلورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</p>
-			</div>
-		</div>
-	</div>
-	
-</div>
+
+				<p class="mt-1 text-xs">{ann.description}</p>
+			</li>
+		{/each}
+	</ul>
+{:else}
+	<Alert>اعلان‌ای پیدا نشد!</Alert>
+{/if}
