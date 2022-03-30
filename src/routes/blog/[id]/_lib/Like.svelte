0a0792c056cc0481ecf8c434de517a24eed9c4bc
babakfp <44144724+babakfp@users.noreply.github.com>
2022-03-29 19:47:21 -0700

    changed svg icon id

--- src/routes/blog/[id]/_lib/Like.svelte
+++ src/routes/blog/[id]/_lib/Like.svelte
@@ -4,8 +4,8 @@
 
 <button class="btn w-10 min-w-10 h-10 p-0 text-sm {isLiked ? 'bg-red-100 text-red-500' : 'bg-gray-100 text-gray-500'}" on:click={_=> isLiked = !isLiked}>
 	{#if isLiked}
-		<svg><use xlink:href="#icon-heart-filled"></use></svg>
+		<svg><use xlink:href="#svg-heart-filled"></use></svg>
 	{:else}
-		<svg><use xlink:href="#icon-heart"></use></svg>
+		<svg><use xlink:href="#svg-heart"></use></svg>
 	{/if}
 </button>
