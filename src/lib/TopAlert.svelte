12de605f124ca1cbd9735223fcd62c2b516890f1
babakfp <44144724+babakfp@users.noreply.github.com>
2022-03-29 18:27:39 -0700

    Hide TopAlert when SideMenu open

--- src/lib/TopAlert.svelte
+++ src/lib/TopAlert.svelte
@@ -1,9 +1,10 @@
 <script>
 	import { writable } from 'svelte-local-storage-store'
 	const showTopAlert = writable('showTopAlert', true)
+	import { isOpen as isSideMenuOpen } from '$store/mobile-menu.js'
 </script>
 
-{#if $showTopAlert}
+{#if $showTopAlert && !$isSideMenuOpen}
 	<div class="z-10 sticky top-0 shadow-b-sm-muted">
 		<div class="flex p-4 bg-white bg-opacity-75 backdrop-blur-md">
 			<button class="h-full p-4 -mr-4 -my-4" on:click={_=> $showTopAlert = false}>
