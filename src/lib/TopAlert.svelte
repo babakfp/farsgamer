2d8216ec082e83b98733210f2bbb0338d5a2d1ed
babakfp <44144724+babakfp@users.noreply.github.com>
2022-03-29 19:04:25 -0700

    TopAlert stuff

--- src/lib/TopAlert.svelte
+++ src/lib/TopAlert.svelte
@@ -1,16 +1,19 @@
 <script>
 	import { writable } from 'svelte-local-storage-store'
-	const showTopAlert = writable('showTopAlert', true)
 	import { isOpen as isSideMenuOpen } from '$store/mobile-menu.js'
+
+	const showTopAlert = writable('showTopAlert', true)
 </script>
 
 {#if $showTopAlert && !$isSideMenuOpen}
-	<div class="flex p-4 bg-white">
-		<button class="h-full p-4 -mr-4 -my-4" on:click={_=> $showTopAlert = false}>
+	<div id="TopAlert" class="flex items-center p-4 bg-white border-b border-gray-100">
+		<button class="p-4 -mr-4 -my-4 duration-150 hover:text-brand hover:scale-110"
+			on:click={_=> $showTopAlert = false}
+		>
 			<i class="fi-rr-cross-small"></i>
 		</button>
-		<div>
-			<p class="text-sm">پشتیبانی 24 ساعته از طریق چت آنلاین در خدمت شما عزیزان. جهت اطلاع از آخرین اخبار <a class="link link--underline" href="javascript:">اینستاگرام فارس گیمر</a> را دنبال کنید.</p>
+		<div class="text-sm">
+			<p>پشتیبانی 24 ساعته از طریق چت آنلاین در خدمت شما عزیزان. جهت اطلاع از آخرین اخبار <a class="link link--underline" href="javascript:">اینستاگرام فارس گیمر</a> را دنبال کنید.</p>
 		</div>
 	</div>
 {/if}
