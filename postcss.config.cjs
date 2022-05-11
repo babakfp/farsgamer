3c200fb677db1280b8681237aaf6ef6da7e3983a
babakfp <44144724+babakfp@users.noreply.github.com>
2022-05-10 20:07:49 -0700

    stufff

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,10 +1,8 @@
-const dev = process.env.NODE_ENV === 'development'
-
 module.exports = {
   plugins: [
 		require('postcss-import'),
     require('tailwindcss/nesting'),
     require('tailwindcss'),
-    !dev && require('autoprefixer'),
+    require('autoprefixer'),
   ],
 }
--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import path from 'path'
 import sveltePreprocess from 'svelte-preprocess'
 import adapter from '@sveltejs/adapter-vercel'
 
-const config = {
+export default {
   kit: {
     adapter: adapter(),
     vite: {
@@ -21,8 +21,6 @@ const config = {
   preprocess: [
 		sveltePreprocess({
 			postcss: true,
-		})
+		}),
 	],
 }
-
-export default config
