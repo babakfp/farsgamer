7bc0294a52047002ae35e6fe75813089983e9380
babakfp <44144724+babakfp@users.noreply.github.com>
2022-06-28 01:34:40 -0700

    Social media stuff

--- src/lib/components/partials/Footer/Social.svelte
+++ src/lib/components/partials/Footer/Social.svelte
@@ -1,11 +1,17 @@
-<div class="flex justify-center gap-4">
-	<a class="flex items-center justify-center w-12 h-12 bg-gray-100 text-gray-400 rounded duration-200 border-2 border-transparent hover:rotate-12 hover:text-gray-500 hover:border-gray-200" href="javascript:">
-		<i class="icon-discord text-xl"></i>
-	</a>
-	<a class="flex items-center justify-center w-12 h-12 bg-gray-100 text-gray-400 rounded duration-200 border-2 border-transparent hover:rotate-12 hover:text-gray-500 hover:border-gray-200" href="javascript:">
-		<i class="icon-telegram text-xl"></i>
-	</a>
-	<a class="flex items-center justify-center w-12 h-12 bg-gray-100 text-gray-400 rounded duration-200 border-2 border-transparent hover:rotate-12 hover:text-gray-500 hover:border-gray-200" href="javascript:">
-		<i class="icon-instagram text-xl"></i>
-	</a>
-</div>
+<script>
+  const items = [
+    { href: 'javascript:', icon: 'icon-discord' },
+    { href: 'javascript:', icon: 'icon-telegram' },
+    { href: 'javascript:', icon: 'icon-instagram' },
+  ]
+</script>
+
+<ul class="flex justify-center gap-4">
+	{#each items as {href, icon}}
+    <li>
+      <a class="flex items-center justify-center w-12 h-12 bg-gray-100 text-gray-400 rounded duration-200 border-2 border-transparent hover:rotate-12 hover:text-gray-500 hover:border-gray-200" {href} target="_blank">
+        <i class="{icon} text-xl"></i>
+      </a>
+    </li>
+  {/each}
+</ul>
