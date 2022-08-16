8a039447f1ef90b282b8d601c33a4fcd6d1acd0b
babakfp <44144724+babakfp@users.noreply.github.com>
2022-08-16 02:19:23 -0700

    fixed topalert and sidenav issue when sidenav open

--- src/lib/components/reusable/TopAlert.svelte
+++ src/lib/components/reusable/TopAlert.svelte
@@ -1,15 +1,16 @@
 <script>
 	import { browser } from '$app/env'
 	import { writable } from 'svelte-local-storage-store'
+	import { isOpen } from '$store/mobile-menu.js'
 	import { isOpen as isSideMenuOpen } from '$store/mobile-menu.js'
 
-	const showTopAlert = writable('showTopAlert', true)
+	const topAlertVisible = writable('topAlertVisible', true)
 </script>
 
-{#if browser && $showTopAlert && !$isSideMenuOpen}
+{#if browser && $topAlertVisible && !$isSideMenuOpen && !$isOpen}
 	<div id="TopAlert" class="z-30 relative flex items-center bg-white border-b border-gray-100">
 		<button class="p-4 duration-150 hover:text-brand transform-scale-fix hover:scale-110"
-			on:click={_=> $showTopAlert = false}
+			on:click={_=> $topAlertVisible = false}
 		>
 			<i class="icon-cross-small"></i>
 		</button>
