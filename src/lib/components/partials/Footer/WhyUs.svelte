f05ac2bd121f764feb1e3b4dd45e489f778a64a0
babakfp <44144724+babakfp@users.noreply.github.com>
2022-08-20 01:52:17 -0700

    textsize

--- src/lib/components/partials/Footer/WhyUs.svelte
+++ src/lib/components/partials/Footer/WhyUs.svelte
@@ -1,6 +1,6 @@
 <ul class="grid justify-evenly gap-4 3xs:grid-cols-2 md:grid-cols-4">
-	<li class="w-full text-center p-4 bg-gray-100 rounded">حفط امنیت اطلاعات</li>
-	<li class="w-full text-center p-4 bg-gray-100 rounded">ضمانت پرداخت</li>
-	<li class="w-full text-center p-4 bg-gray-100 rounded">پشتیبانی ۲۴/۷</li>
-	<li class="w-full text-center p-4 bg-gray-100 rounded">ثبت سفارش فوری</li>
+	<li class="w-full p-4 text-center text-sm bg-gray-100 rounded">حفط امنیت اطلاعات</li>
+	<li class="w-full p-4 text-center text-sm bg-gray-100 rounded">ضمانت پرداخت</li>
+	<li class="w-full p-4 text-center text-sm bg-gray-100 rounded">پشتیبانی ۲۴/۷</li>
+	<li class="w-full p-4 text-center text-sm bg-gray-100 rounded">ثبت سفارش فوری</li>
 </ul>
