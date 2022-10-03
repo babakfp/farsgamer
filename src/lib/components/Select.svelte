b041136c94e4eeca10de04b9d247e60ce8b9a179
babakfp <44144724+babakfp@users.noreply.github.com>
2022-10-03 10:39:57 -0700

    Select component

---
+++ src/lib/components/Select.svelte
@@ -0,0 +1,51 @@
+<script>
+	import Select from 'svelte-select'
+	
+	export let items
+	export let placeholder
+	export let groupBy
+	export let noOptionsMessage = 'گذینه‌ای وجود نیست!'
+</script>
+
+<div class="relative w-full">
+	<Select {items} {groupBy} {placeholder} {noOptionsMessage} />
+	<i class="icon-caret-down absolute top-1/2 -translate-y-1/2 left-4 text-gray-300 pointer-events-none"></i>
+</div>
+
+<style lang="postcss" global>
+	:root {
+		--borderRadius: theme('borderRadius.sm');
+		--border: 2px solid theme('colors.gray.300');
+		--itemIsActiveBG: theme('colors.brand.DEFAULT');
+		--listBorderRadius: theme('borderRadius.sm');
+		--selectedItemPadding: 0 0 0 1rem;
+		--clearSelectWidth: 2rem;
+		--itemHoverBG: theme(colors.gray.100);
+		--clearSelectColor: theme(colors.gray.300);
+	}
+	.selectContainer {
+		@apply text-sm;
+	}
+	.selectContainer input,
+	.selectContainer .item {
+		@apply !cursor-pointer;
+	}
+	.selectContainer .item {
+		@apply duration-150;
+	}
+	.selectContainer .groupItem {
+		@apply pr-10;
+	}
+	.selectContainer .clearSelect {
+		@apply flex items-center justify-center !right-auto !text-gray-300 cursor-pointer duration-100 ease-in-out hover:!text-brand hover:text-opacity-80;
+		height: calc(100% + 4px);
+		left: 2rem;
+		top: -2px !important;
+	}
+	.selectContainer .clearSelect svg {
+		@apply w-5;
+	}
+	.selectContainer .listContainer {
+		@apply !top-12 !bottom-auto;
+	}
+</style>
--- src/routes/shop/_lib/SelectBoxes.svelte
+++ src/routes/shop/_lib/SelectBoxes.svelte
@@ -1,5 +1,5 @@
 <script>
-	import Select from 'svelte-select'
+	import Select from '$components/Select.svelte'
 	
 	const categoryItems = [
 		{ group: 'فورتنایت | Fortnite', label: 'ویباکس | V-Bucks', value: 'v-bucks' },
@@ -27,55 +27,6 @@
 </script>
 
 <div class="grid gap-4 3xs:flex">
-	<div class="relative w-full">
-		<Select
-			items={categoryItems} {groupBy}
-			placeholder="دسته‌بندی محصولات" noOptionsMessage="گذینه‌ای موجود نیست"
-		/>
-		<i class="icon-caret-down absolute top-1/2 -translate-y-1/2 left-4 text-gray-300"></i>
-	</div>
-	
-	<div class="relative w-full">
-		<Select
-			items={sortItems} {groupBy}
-			placeholder="مرتب سازی" noOptionsMessage="گذینه‌ای موجود نیست"
-		/>
-		<i class="icon-caret-down absolute top-1/2 -translate-y-1/2 left-4 text-gray-300"></i>
-	</div>
+	<Select items={categoryItems} {groupBy} placeholder="دسته‌بندی محصولات" />
+	<Select items={sortItems} {groupBy} placeholder="مرتب سازی" />
 </div>
-
-<style lang="postcss">
-	:root {
-		--borderRadius: theme('borderRadius.sm');
-		--border: 2px solid theme('colors.gray.300');
-		--itemIsActiveBG: theme('colors.brand.DEFAULT');
-		--listBorderRadius: theme('borderRadius.sm');
-		--selectedItemPadding: 0 0 0 1rem;
-		--clearSelectWidth: 2rem;
-		--itemHoverBG: rgb(61 66 223 / 10%);
-	}
-
-	:global(.selectContainer) {
-		@apply text-sm;
-	}
-
-	:global(.selectContainer input),
-	:global(.selectContainer .item) {
-		@apply !cursor-pointer;
-	}
-	:global(.selectContainer .groupItem) {
-		@apply pr-10;
-	}
-	:global(.selectContainer .clearSelect) {
-		@apply flex items-center justify-center !right-auto !text-gray-300 cursor-pointer duration-100 ease-in-out hover:!text-brand hover:text-opacity-80;
-		height: calc(100% + 4px);
-		left: 2rem;
-		top: -2px !important;
-	}
-	:global(.selectContainer .clearSelect svg) {
-		@apply w-5;
-	}
-	:global(.selectContainer .listContainer) {
-		@apply !top-12 !bottom-auto;
-	}
-</style>
