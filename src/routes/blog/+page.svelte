570616ff91e57029c7378d8ccb5b40f9f7444cc8
babakfp <44144724+babakfp@users.noreply.github.com>
2022-10-05 12:15:53 -0700

    fixed blog posts 8 items per page

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -12,9 +12,9 @@
 </svelte:head>
 
 <div class="grid gap-4 grid-cols-2 md:gap-6 md:grid-cols-3 2lg:grid-cols-4">
-	{#each getPaginationItems(posts, currentPageNumber) as post (post.id)}
+	{#each getPaginationItems(posts, currentPageNumber, 8) as post (post.id)}
 		<PostCard id={post.id} title={post.title} featuredImage={post.featuredImage} />
 	{/each}
 </div>
 
-<Pagination {currentPageNumber} maxNumberOfPage={getPaginationMaxNumberOfPages(posts)} baseUrl={'/blog'} />
+<Pagination {currentPageNumber} maxNumberOfPage={getPaginationMaxNumberOfPages(posts, 8)} baseUrl={'/blog'} />
