fbee657287d3d4b83adee7a0ea8579aa35a2295e
babakfp <44144724+babakfp@users.noreply.github.com>
2022-10-19 15:50:07 -0700

    added focus style to sidebar and dashboard menu items

--- src/routes/dashboard/_lib/NavItem.svelte
+++ src/routes/dashboard/_lib/NavItem.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <li>
-	<a class="relative flex items-center gap-3 py-3 pl-6 pr-5 hover:text-brand
+	<a class="relative flex items-center gap-3 py-3 pl-6 pr-5 hover:text-brand focus-visible:text-brand
     {href === $page.url.pathname && 'text-brand cursor-default'}"
 		{href}>
 		<i class="{icon} w-8 h-8 flex items-center justify-center text-lg opacity-75" />
