2af8d72f6967758b279e0441feea81223f474ba8
babakfp <44144724+babakfp@users.noreply.github.com>
2022-10-20 06:46:56 -0700

    a11y

--- src/lib/partials/Header/announcements/Index.svelte
+++ src/lib/partials/Header/announcements/Index.svelte
@@ -31,11 +31,11 @@
 	</button>
 
 	<OutClick on:outclick={()=> isOpen = false} excludeByDomNode={[toggle]}>
-		<div class:hidden={!isOpen} class="z-20 absolute inset-x-4 top-full -mt-2 overflow-hidden bg-white border border-gray-100 rounded-sm 3xs:left-0 3xs:right-auto 3xs:w-80 xs:w-88" style="box-shadow: 0 1rem 3rem rgb(0 0 0 / 18%)">
+		<div class="z-20 absolute inset-x-4 top-full -mt-2 overflow-hidden bg-white border border-gray-100 rounded-sm duration-150 hide -translate-y-8 {isOpen && 'show !translate-y-0'} 3xs:left-0 3xs:right-auto 3xs:w-80 xs:w-88" style="box-shadow: 0 1rem 3rem rgb(0 0 0 / 18%)">
 
 			<div class="flex items-center justify-between p-3 border-b border-gray-100 text-sm">
 				<span>اعلانات</span>
-				<a class="text-xs link" href="/dashboard/announcements">مشاهده همه</a>
+				<a class="link text-xs | -ml-3 -my-3 pl-3 py-3" href="/dashboard/announcements">مشاهده همه</a>
 			</div>
 
 			<div class="py-2">
