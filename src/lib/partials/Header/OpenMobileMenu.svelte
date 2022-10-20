2bb05796328f12904f54ec729b40fb43b9786375
babakfp <44144724+babakfp@users.noreply.github.com>
2022-10-20 08:48:48 -0700

    stuff

--- src/lib/partials/Header/OpenMobileMenu.svelte
+++ src/lib/partials/Header/OpenMobileMenu.svelte
@@ -4,7 +4,7 @@
 
 <div class="h-full border-l border-gray-100">
   <button on:click={()=> $isOpen = !$isOpen} class="group js-burger-toggle-btn flex items-center justify-center w-header h-header -mr-4 duration-200 active:scale-95" aria-label="{$isOpen ? 'باز کردن' : 'بستن'} منو">
-    <i class="icon-apps text-gray-600 duration-300 ease-in-out {$isOpen && 'rotate-45'} group-focus-visible:text-brand sm:text-lg" />
+    <i class="icon-apps text-gray-600 duration-300 ease-in-out {$isOpen && 'rotate-45'} group-hover:text-brand group-focus-visible:text-brand sm:text-lg" />
   </button>
 </div>
 
