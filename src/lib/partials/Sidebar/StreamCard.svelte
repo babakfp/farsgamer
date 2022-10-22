1a120eb4b5a891f54740085e4fb9a0bb59adf3f4
babakfp <44144724+babakfp@users.noreply.github.com>
2022-10-22 10:53:27 -0700

    Added missing aspect-ratio to stream boxes

--- src/lib/partials/Sidebar/StreamCard.svelte
+++ src/lib/partials/Sidebar/StreamCard.svelte
@@ -8,7 +8,7 @@
 <li>
   <a class="group flex items-center justify-between gap-2 py-3 px-4 bg-gray-50 border border-gray-100 rounded duration-200 ease-in-out outline-card" href={liveStreamLink} target="_blank" rel="noreferrer noopener nofollow">
     <div class="flex items-center gap-2">
-      <img class="w-10 border-2 border-white rounded-full drop-shadow-lg duration-200 ease-in-out" src={avatar} alt loading="lazy">
+      <img class="w-10 aspect-square border-2 border-white rounded-full drop-shadow-lg duration-200 ease-in-out" src={avatar} alt loading="lazy">
       <div>
         <h6 class="font-bold text-sm">{username}</h6>
         <p class="text-xs">{game}</p>
