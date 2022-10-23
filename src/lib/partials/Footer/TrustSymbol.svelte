1d2a2a500915238d4221276e12c712e2d63536cd
babakfp <44144724+babakfp@users.noreply.github.com>
2022-10-22 18:31:33 -0700

    Added new comps

---
+++ src/lib/partials/Footer/TrustSymbol.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let src
+	export let alt
+</script>
+
+<li>
+	<a class="flex | w-20 | border border-gray-100 | rounded overflow-hidden | duration-300 ease-out transition-transform
+		hover:scale-150 hover:z-1 hover:shadow-lg lg:hover:scale-125
+		focus-visible:scale-150 focus-visible:z-1 focus-visible:shadow-lg lg:focus-visible:scale-125
+	" href="javascript:">
+		<img {src} {alt} loading="lazy" />
+	</a>
+</li>
--- src/lib/partials/Footer/TrustSymbols.svelte
+++ src/lib/partials/Footer/TrustSymbols.svelte
@@ -1,22 +1,12 @@
 <script>
-	const images = [
-		{ alt: 'نماد تجارت الکترونیکی اینماد - به آدرس enamad.ir', src: '/img/trust-symbols/enamad.ir.png' },
-		{ alt: 'نماد ساماندهی محتوای فضای مجازی - به آدرس samandehi.ir', src: '/img/trust-symbols/samandehi.ir.png' },
-		{ alt: 'نماد پنیاد ملی بازی‌های رایانه‌ای - به آدرس ircg.ir', src: '/img/trust-symbols/ircg.ir.png' },
-		{ alt: 'نماد اتحادیه کشوری کسب‌وکارهای مجازی - به آدرس ecunion.ir', src: '/img/trust-symbols/ecunion.ir.png' },
-		{ alt: 'نماد درگاه پرداخت واسط زرین‌پال - به آدرس zarinpal.com', src: '/img/trust-symbols/zarinpal.com.png' },
-		{ alt: 'نماد درگاه پرداخت واسط پی - به آدرس pay.ir', src: '/img/trust-symbols/pay.ir.png' },
-	]
+	import TrustSymbol from './TrustSymbol.svelte'
 </script>
 
-<div class="flex flex-wrap justify-center gap-4">
-	{#each images as img}
-    <!-- svelte-ignore a11y-invalid-attribute -->
-		<a class="flex w-20 border border-gray-100 rounded overflow-hidden duration-300 ease-out transition-transform
-			hover:scale-150 hover:z-1 hover:shadow-lg lg:hover:scale-125
-			focus-visible:scale-150 focus-visible:z-1 focus-visible:shadow-lg lg:focus-visible:scale-125
-		" href="javascript:">
-			<img src={img.src} alt={img.alt} loading="lazy">
-		</a>
-	{/each}
-</div>
+<ul class="flex flex-wrap justify-center gap-4">
+	<TrustSymbol src="/img/trust-symbols/enamad.ir.png" alt="نماد تجارت الکترونیکی اینماد - به آدرس enamad.ir" />
+	<TrustSymbol src="/img/trust-symbols/samandehi.ir.png" alt="نماد ساماندهی محتوای فضای مجازی - به آدرس samandehi.ir" />
+	<TrustSymbol src="/img/trust-symbols/ecunion.ir.png" alt="نماد اتحادیه کشوری کسب‌وکارهای مجازی - به آدرس ecunion.ir" />
+	<TrustSymbol src="/img/trust-symbols/ircg.ir.png" alt="نماد پنیاد ملی بازی‌های رایانه‌ای - به آدرس ircg.ir" />
+	<TrustSymbol src="/img/trust-symbols/pay.ir.png" alt="نماد درگاه پرداخت واسط پی - به آدرس pay.ir" />
+	<TrustSymbol src="/img/trust-symbols/zarinpal.com.png" alt="نماد درگاه پرداخت واسط زرین‌پال - به آدرس zarinpal.com" />
+</ul>
