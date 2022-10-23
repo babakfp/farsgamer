2ed90c0c54837d34abc53db67b75ae5932322493
babakfp <44144724+babakfp@users.noreply.github.com>
2022-10-22 18:37:14 -0700

    stuff

--- src/lib/partials/Footer/Social.svelte
+++ src/lib/partials/Footer/Social.svelte
@@ -1,15 +1,15 @@
 <script>
   const items = [
-    { label: 'لینک دیسکورد', icon: 'icon-discord', href: 'javascript:' },
-    { label: 'لینک تلگرام', icon: 'icon-telegram', href: 'javascript:' },
-    { label: 'لینک اینستاگرام', icon: 'icon-instagram', href: 'javascript:' },
+    { label: 'لینک دیسکورد', icon: 'icon-discord' },
+    { label: 'لینک تلگرام', icon: 'icon-telegram' },
+    { label: 'لینک اینستاگرام', icon: 'icon-instagram' },
   ]
 </script>
 
 <ul class="flex justify-center gap-4">
 	{#each items as item}
     <li>
-      <a class="btn btn--light btn--outline px-0 | w-12 h-12 rounded-full text-gray-500 !scale-100" href={item.href} target="_blank" rel="noreferrer noopener" aria-label={item.label}>
+      <a class="btn btn--light btn--outline px-0 | w-12 h-12 rounded-full text-gray-500 !scale-100" href="javascript:" target="_blank" rel="noreferrer noopener" aria-label={item.label}>
         <i class="{item.icon} text-xl" />
       </a>
     </li>
