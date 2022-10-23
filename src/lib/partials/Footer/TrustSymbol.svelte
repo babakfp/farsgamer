7e827399c2c7e292e50049a0a370b01310066ece
babakfp <44144724+babakfp@users.noreply.github.com>
2022-10-22 18:54:30 -0700

    stuff

--- src/lib/partials/Footer/TrustSymbol.svelte
+++ src/lib/partials/Footer/TrustSymbol.svelte
@@ -4,9 +4,9 @@
 </script>
 
 <li>
-	<a class="flex | w-20 | border border-gray-100 | rounded overflow-hidden | duration-300 ease-out transition-transform
-		hover:scale-150 hover:z-1 hover:shadow-lg lg:hover:scale-125
-		focus-visible:scale-150 focus-visible:z-1 focus-visible:shadow-lg lg:focus-visible:scale-125
+	<a class="flex | w-20 | border border-gray-100 | rounded overflow-hidden | duration-300 ease-out
+		hover:scale-125 hover:z-1 hover:drop-shadow-sm
+		focus-visible:scale-125 focus-visible:z-1 focus-visible:drop-shadow-sm
 	" href="javascript:">
 		<img {src} {alt} loading="lazy" />
 	</a>
